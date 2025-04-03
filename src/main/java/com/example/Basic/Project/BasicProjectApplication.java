964456aae28e6c71a03f278b676b53bdcda55c9e
pawan <pa211nagar@gmail.com>
2025-04-03 11:50:44 +0530

    learn about the developing Rest APIs

---
+++ src/main/java/com/example/Basic/Project/BasicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Basic.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicProjectApplication.class, args);
+	}
+
+}
